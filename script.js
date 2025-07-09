4bb8734236e785f1ab97eb1b0228cd8da820c4ad
entropyftc <entropyftc@gmail.com>
2025-07-09 10:21:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.mobile-menu').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
